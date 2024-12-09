f4bb53abeb0302e07fcc0c346dbc51ffc53ce835
Pasha <100295341+iaaka@users.noreply.github.com>
2024-12-09 14:46:19 +0000

    Update sumCovs.R
    fix case length(l)==1

--- R/sumCovs.R
+++ R/sumCovs.R
@@ -1,10 +1,12 @@
 #' Returns the summary coverage for region
-#' @param l region
+#' @param l list of coverages for same region to be summed
 #'
 #' @export
 
 sumCovs = function(l){
   r = l[[1]]
+  if(length(l)==1)
+    return(r);
   juncs = unique(do.call(rbind,unname(lapply(l,function(c)c$juncs[,1:3]))))
   juncs$score = 0
   juncs[rownames(r$juncs),'score'] = r$juncs$score
